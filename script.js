a25f39719fb2a991eb43b56d9c7a152a95fcea38
LewisWindsor <100640865+LewisWindsor@users.noreply.github.com>
2024-01-22 23:53:22 -0500

    Updated Sprite Frame Sizing

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
-const spriteWidth = 575
+const spriteWidth = 575;
+const spriteHeight = 523;
 //let x = 0;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(x, 50, 100, 100);
-    ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     //ctx.drawImage(playerImage, 50, 50);
     requestAnimationFrame(animate);
 };
