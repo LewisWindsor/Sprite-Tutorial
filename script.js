a7c6c9f44a394557826f9fed342060ed76d217f7
LewisWindsor <100640865+LewisWindsor@users.noreply.github.com>
2024-01-23 00:00:22 -0500

    Updated sprite refresh

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
 //let x = 0;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(x, 50, 100, 100);
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (frameX < 7) frameX++;
+    else frameX = 0;
     //ctx.drawImage(playerImage, 50, 50);
     requestAnimationFrame(animate);
 };
