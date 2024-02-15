3b242e09ec85443faa968aef563b5342756c0918
LewisWindsor <100640865+LewisWindsor@users.noreply.github.com>
2024-02-15 17:27:52 -0500

    fixed blinking with staggerFrames var

--- script.js
+++ script.js
@@ -9,14 +9,21 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+let staggerFrames = 5;
 //let x = 0;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(x, 50, 100, 100);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (frameX < 7) frameX++;
+    if (gameFrame % staggerFrames == 0){
+        if (frameX < 6) frameX++;
     else frameX = 0;
+    }
+    
+
+    gameFrame++;
     //ctx.drawImage(playerImage, 50, 50);
     requestAnimationFrame(animate);
 };
