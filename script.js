d87b4f6f3c779ac8db0a37a1ecd9f4e34b0e4fba
LewisWindsor <100640865+LewisWindsor@users.noreply.github.com>
2024-02-15 18:02:43 -0500

    Added additional code for array set with irregular sprite sheets

--- script.js
+++ script.js
@@ -11,6 +11,29 @@ let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 let staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0 ; j < state.frames; j++){
+        let positonX = j * spriteWidth;
+        let positonY = index * spriteHeight;
+        frames.loc.push({x: positonX, y: positonY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
